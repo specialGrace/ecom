97388914600432f41286ebf8fe94712b24cc79cf
specialGrace <tunmiseadebayo9@gmail.com>
2023-11-09 16:27:57 +0100

    Footer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
 
-
-
 function Layout(props) {
   return (
       <div className="flex flex-col min-h-screen" >
